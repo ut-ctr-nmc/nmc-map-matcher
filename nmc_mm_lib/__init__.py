dbb46b024561cbd0664a9883544b0ad25785396f
Kenneth A Perrine <kap2357@nmc-compute1.ctr.utexas.edu>
2015-01-18 22:24:58 -0600

    Fixed module import problem for nmc_mm_lib by adding an empty module initializer.

---
+++ nmc_mm_lib/__init__.py
@@ -0,0 +1,25 @@
+"""
+__init__.py: Initializer for nmc_mm_lib module directory.
+@author: Kenneth Perrine
+@contact: kperrine@utexas.edu
+@organization: Network Modeling Center, Center for Transportation Research,
+    Cockrell School of Engineering, The University of Texas at Austin
+@version: 1.0
+
+@copyright: (C) 2014, The University of Texas at Austin
+@license: GPL v3
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
