1c0e85bdfd01d0e49bb3cfb170fe747f051c2082
Ken Perrine <kperrine@utexas.edu>
2015-06-15 10:43:54 -0500

    Fixes for arcgiscsv support.

--- arcgiscsv_extracted.py
+++ arcgiscsv_extracted.py
@@ -25,9 +25,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 from __future__ import print_function
-from datetime import datetime
+from datetime import datetime, timedelta
 from nmc_mm_lib import gtfs, vista_network, path_engine
-import operator, sys
+import operator, sys, csv
 
 # A module that deals with reading CSV files extracted from a GDB format.
 DIVISOR = 1000L * long(1 << 48)
@@ -54,38 +54,37 @@ def fillFromFile(filename, GPS):
     ret = {}
     "@type ret: dict<str, list<gtfs.ShapesEntry>>"
     with open(filename, 'r') as inFile:
-        # Sanity check:
-        fileLine = inFile.readline()
-        if not fileLine.startswith("FID,OBJECTID,StudyId,GISFile,Datafile,UserId,DeviceId,VideoRecor,"
-                                   "UtcDateTim,GPSDateTim,GPSDateTime,GPSDate,GPSTime,Bearing,SpeedMPH,HDOP,"
-                                   "Elevation,Latitude,Longitude,TimePeriodId,RouteId"):
-            print("ERROR: The ArcGIS CSV file %s doesn't have the expected header." % filename, file = sys.stderr)
-            return None
+        csvReader = csv.DictReader(inFile)
+
+        firstRun = True
+        for fileLine in csvReader:
+            if firstRun:
+                # Sanity check:
+                if not ("RouteId" in fileLine and "Datafile" in fileLine and "Latitude" in fileLine
+                        and "Longitude" in fileLine and "OBJECTID" in fileLine and "UtcDateTim" in fileLine):
+                    print("ERROR: The ArcGIS CSV file %s doesn't have the expected header." % filename, file = sys.stderr)
+                    return None
+                firstRun = False
         
-        # Go through the lines of the file:
-        for fileLine in inFile:
-            if len(fileLine) > 0:
-                lineElems = fileLine.split(',')
-# Increment the indices by 1!
-                routeID = int(lineElems[18])
-                if routeID > 0:
-                    identifier = lineElems[3] + "(" + lineElems[18] + ")"
-                    newEntry = gtfs.ShapesEntry(identifier, int(lineElems[0]), float(lineElems[15]),
-                                        float(lineElems[16]), False)
-                    (newEntry.pointX, newEntry.pointY) = GPS.gps2feet(newEntry.lat, newEntry.lng)
-                    newEntry.time = datetime.strptime(lineElems[8], '%m/%d/%Y %H:%M:%S %p')
-                    
+            routeID = int(fileLine["RouteId"].replace(",", ""))
+            if routeID > 0:
+                identifier = fileLine["Datafile"] + "(" + str(routeID) + ")"
+                newEntry = gtfs.ShapesEntry(identifier, int(fileLine["OBJECTID"].replace(",", "")), float(fileLine["Latitude"]),
+                                    float(fileLine["Longitude"]), False)
+                (newEntry.pointX, newEntry.pointY) = GPS.gps2feet(newEntry.lat, newEntry.lng)
+                newEntry.time = datetime.strptime(fileLine["UtcDateTim"], '%m/%d/%Y %H:%M:%S %p')
+                
 # Temporary set to CST: 
-                    timeDelta = datetime.timedelta(hours=-6)
-                    newEntry.time += timeDelta
-                    
-                    # Keep only the time:
-                    newEntry.time = datetime.strptime("%02d:%02d:%02d" % (newEntry.time.hour, newEntry.time.minute,
-                                                                          newEntry.time.second), '%H:%M:%S')
-                    
-                    if newEntry.shapeID not in ret:
-                        ret[newEntry.shapeID] = []
-                    ret[newEntry.shapeID].append(newEntry)
+                timeDelta = timedelta(hours=-6)
+                newEntry.time += timeDelta
+                
+                # Keep only the time:
+                newEntry.time = datetime.strptime("%02d:%02d:%02d" % (newEntry.time.hour, newEntry.time.minute,
+                                                                      newEntry.time.second), '%H:%M:%S')
+                
+                if newEntry.shapeID not in ret:
+                    ret[newEntry.shapeID] = []
+                ret[newEntry.shapeID].append(newEntry)
 
     # Ensure that the lists are sorted:
     for shapesEntries in ret.values():
@@ -100,14 +99,14 @@ def pathMatch(dbServer, networkName, userName, password, filename, limitMap = No
     pointSearchRadius = 1000    # "k": Radius (ft) to search from GTFS point to perpendicular VISTA links
     pointSearchPrimary = 350    # "k_p": Radius (ft) to search from GTFS point to new VISTA links    
     pointSearchSecondary = 200  # "k_s": Radius (ft) to search from VISTA perpendicular point to previous point
-    limitLinearDist = 3800      # Path distance (ft) to allow new proposed paths from one point to another
-    limitDirectDist = 3500      # Radius (ft) to allow new proposed paths from one point to another
-    limitDirectDistRev = 500    # Radius (ft) to allow backtracking on an existing link (e.g. parking lot)
+    limitLinearDist = 500      # Path distance (ft) to allow new proposed paths from one point to another
+    limitDirectDist = 400      # Radius (ft) to allow new proposed paths from one point to another
+    limitDirectDistRev = 300    # Radius (ft) to allow backtracking on an existing link (e.g. parking lot)
     distanceFactor = 1.0        # "f_d": Cost multiplier for Linear path distance
     driftFactor = 1.5           # "f_r": Cost multiplier for distance from GTFS point to its VISTA link
     nonPerpPenalty = 1.5        # "f_p": Penalty multiplier for GTFS points that aren't perpendicular to VISTA links
-    limitClosestPoints = 12     # "q_p": Number of close-proximity points that are considered for each GTFS point 
-    limitSimultaneousPaths = 8  # "q_e": Number of proposed paths to maintain during pathfinding stage
+    limitClosestPoints = 8     # "q_p": Number of close-proximity points that are considered for each GTFS point 
+    limitSimultaneousPaths = 6  # "q_e": Number of proposed paths to maintain during pathfinding stage
     
     maxHops = 12                # Maximum number of VISTA links to pursue in a path-finding operation
     
@@ -121,7 +120,7 @@ def pathMatch(dbServer, networkName, userName, password, filename, limitMap = No
     
     # Read in the GPS track information:
     print("INFO: Read GDB GPS track...", file = sys.stderr)
-    gpsTracks = fillFromFile(filename, vistaGraph.GPS)
+    gpsTracks = fillFromFile(filename, vistaGraph.gps)
     
     # Initialize the path-finder:
     pathFinder = path_engine.PathEngine(pointSearchRadius, pointSearchPrimary, pointSearchSecondary, limitLinearDist,
